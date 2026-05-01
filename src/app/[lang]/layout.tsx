4a31a31634735dc903eae82c9fb3de61b87335bc
rkamika <rodrigo.kamikihara@gmail.com>
2026-05-01 11:35:22 -0300

    chore: add microsoft clarity script to layout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -45,6 +45,15 @@ export default async function RootLayout({
             }}
           />
         )}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "wkcb26fnxs");`,
+          }}
+        />
       </head>
       <body className="antialiased bg-background text-foreground transition-colors duration-300">
         {gtmId && (
